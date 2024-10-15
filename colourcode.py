daab05c7c7fe52aab56ecdfff14ddaeee3670a33
JoeMartinRince <joemartinrince2028@cs.sjcetpalai.ac.in>
2024-10-15 15:22:28 +0530

    Add files via upload

---
+++ colourcode.py
@@ -0,0 +1,17 @@
+'''
+Python program to assign different colours for different grades
+AUTHOR : Joe Martin Rince
+DATE : 15-10-2024
+VERSION : 1.0
+'''
+grade=input("Enter your grade : ")
+if grade == "R":
+    print("Your colour is red")
+elif grade == "G":
+    print("Your colour is green")
+elif grade == "B":
+    print("Your colour is blue")
+elif grade == "v":
+    print("Your colour is violet")
+else:
+    print("wrong code")
