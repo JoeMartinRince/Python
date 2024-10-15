c4a404173bccd218707d28670e0be20341c3dbcd
JoeMartinRince <joemartinrince2028@cs.sjcetpalai.ac.in>
2024-10-15 14:47:55 +0530

    Add files via upload

---
+++ smallest of three numbers.py
@@ -0,0 +1,14 @@
+"""
+AUTHOR : Joe Martin Rince
+DATE : 15-10-2024
+VERSION :1.0
+"""
+num1=int(input("Enter num1 :" ))
+num2=int(input("Enter num2 :" ))
+num3=int(input("Rnter num3 :" ))
+if num2>num1 and num3>num1:
+    print(",num1,"" is the smallest of all ")
+elif num1>num2 and num3>num2:
+    print(",num2,""is the smallest of all ")
+else:
+    print(",num3,""is the smallest of all ")
