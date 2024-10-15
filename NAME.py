605eabee23d8341ebbeed667c78e0394ea9b252d
JoeMartinRince <joemartinrince2028@cs.sjcetpalai.ac.in>
2024-10-15 16:17:42 +0530

    Add files via upload

---
+++ NAME.py
@@ -0,0 +1,8 @@
+first_name=input("Enter your first name: ")
+last_name=input("Enter your last name: ")
+full_name=first_name+last_name
+print(full_name)
+length=len(first_name)
+print(length)
+extracted_first_name=full_name[:length]
+print(extracted_first_name)
