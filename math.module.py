e81bab48907f55f43410ef0cca7f00e984908489
JoeMartinRince <joemartinrince2028@cs.sjcetpalai.ac.in>
2024-10-28 22:37:01 +0530

    Add files via upload

---
+++ math.module.py
@@ -0,0 +1,14 @@
+"""
+AUTHOR : Joe Martin Rince
+DATE : 28-10-2024
+PYTHON PROGRAM FOR MATH MODULE
+"""
+
+import math
+number=int(input("Enter your number : "))
+sqrt_result = math.sqrt(number)
+print("The square of the given number is ",sqrt_result,"")
+factorial_result=math.factorial(number)
+print("The factorial of the number is ",factorial_result,"")
+square_result = number ** 2
+print("The square of the number is ", square_result ,"")
