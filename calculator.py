bd56fcd3e078fa9731d4620123ac549ea4f805bd
JoeMartinRince <joemartinrince2028@cs.sjcetpalai.ac.in>
2024-11-15 06:54:26 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,23 @@
+"""
+AUTHOR : JOE MARTIN RINCE
+DATEC:14-11-2024
+"""
+from itertools import product
+
+num1=int(input("Enter your number 1 : "))
+num2=int(input("Enter your number 2 :"))
+operation=input("Enter your operation , '+' , '_' , '*' or '/': ")
+if operation == '+':
+    sum1=(num1+num2)
+    print(sum1)
+elif operation=='_':
+    difference=(num1-num2)
+    print(difference)
+elif operation=='*':
+    product1=(num1*num2)
+    print(product1)
+elif operation=='/':
+    quotient=(num1/num2)
+    print(quotient)
+else :
+    print("wrong operation")
