90a646f320dc7ed4cff346717b8967e56ccf6b08
JoeMartinRince <joemartinrince2028@cs.sjcetpalai.ac.in>
2024-11-19 08:33:48 +0530

    rockpaperscissors.py

--- rockpaperscissors.py
+++ rockpaperscissors.py
@@ -2,24 +2,33 @@
 AUTHOR: JOE MARTIN RINCE
 DATE :18-11-2024
 """
+Your script has indentation and logical errors. Here's the corrected version:
+
 import random
 
-text="You Lose !"
-choices=["rock","paper","scissors"]
-while True:
-    num=int(input("Enter 0 for rock, 1 for paper, and 2 for scissors:  "))
-    no=random.randint(0,2)
-    print(f"Computer choice : {choices[no]}")
-    if num>2 or num<0:
-    print ("Invalid response ")
+text = "You Lose!"
+choices = ["rock", "paper", "scissors"]
 
-    elif choices[num]==0 and choices[no]==1:
-        print(text)
-    elif choices[num]==1 and choices[no]==2:
-        print(text)
-    elif choices[num]==2 and choices[no]==0:
-        print(text)
-    elif choices[num]==choices[no]:
-        print("Tie")
-    else:
-        print("You Win!")
+while True:
+    
+        num = int(input("Enter 0 for rock, 1 for paper, and 2 for scissors: "))
+        
+       
+        if num < 0 or num > 2:
+            print("Invalid response. Please enter 0, 1, or 2.")
+            continue
+        
+        no = random.randint(0, 2)
+        print(f"Computer choice: {choices[no]}")
+
+        
+        if num == no:
+            print("Tie")
+        elif (num == 0 and no == 2) or (num == 1 and no == 0) or (num == 2 and no == 1):
+            print("You Win!")
+        else:
+            print(text)
+    except ValueError:
+        print("Invalid input! Please enter a number (0, 1, or 2).")
+
+
