8b97a6fef7f7bb9d68ea6e2ceab771dfdec2e747
JoeMartinRince <joemartinrince2028@cs.sjcetpalai.ac.in>
2024-11-30 09:43:05 +0530

    checkmobile_number.py

--- checkmobile_number.py
+++ checkmobile_number.py
@@ -4,7 +4,7 @@ Bate : 30-11-2024
 """
 number1=input("Enter a number :")
 def check_number():
-    if len(number1)==10 and {number1[0]==9 or number1[0]==7 or number1[0]==8}:
+    if len(number1)==10 and number1[0]=="9" or number1[0]=="7" or number1[0]=="8":
         print("valid number ")
     else:
         print("Invalid number")
