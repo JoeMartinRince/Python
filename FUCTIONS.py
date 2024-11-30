364e1fe8af6847727b9406f69471c0044efc2f79
JoeMartinRince <joemartinrince2028@cs.sjcetpalai.ac.in>
2024-11-30 09:39:55 +0530

    Add files via upload

---
+++ FUCTIONS.py
@@ -0,0 +1,17 @@
+"""
+AUTHOR : Joe Martin Rince
+DATE:30-11-2024
+
+"""
+def add_two_numbers(number1,number2):
+    sum1=number1 +number2
+    print(sum1)
+
+add_two_numbers(5,7)
+
+def multiply_two_numbers(number1,number2):
+    product=number1*number2
+    print(product)
+
+
+multiply_two_numbers(5,7)
---
+++ mobile_number.py
@@ -0,0 +1,12 @@
+"""
+AUTHOR : Joe Martin Rince
+Bate : 30-11-2024
+"""
+number1=input("Enter a number :")
+def check_number():
+    if len(number1)==10 and {number1[0]==9 or number1[0]==7 or number1[0]==8}:
+        print("valid number ")
+    else:
+        print("Invalid number")
+
+check_number()
