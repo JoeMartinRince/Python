a258f114b89130cd05cb78d2ad285fb7be96bc76
JoeMartinRince <joemartinrince2028@cs.sjcetpalai.ac.in>
2024-12-17 03:59:06 +0530

    exp2.py

--- exp2.py
+++ exp2.py
@@ -1,6 +1,8 @@
 """
 AUTHOR : JOE MARTIN RINCE
 DATE : 05-10-2024
+Write a Python program that stores a string in a variable. Extract a specific part of the string (substring) and then concatenate it with another string. 
+Finally, display the new string.
 """
 str1="Hello ,World"
 str2=str1[7:12]
