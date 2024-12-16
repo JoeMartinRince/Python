510e8ef242a98049156e231059ec661d9d9a7c30
JoeMartinRince <joemartinrince2028@cs.sjcetpalai.ac.in>
2024-12-17 04:04:43 +0530

    string.py

--- NAME.py
+++ NAME.py
@@ -1,3 +1,6 @@
+'''AUTHOR:Joe Martin Rince
+08/10/2024
+Create, concatenate, and print a string and access a sub-string from a given string.'''
 first_name=input("Enter your first name: ")
 last_name=input("Enter your last name: ")
 full_name=first_name+last_name
