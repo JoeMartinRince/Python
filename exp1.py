020bbed13850aff7b5c5261038f2356858958f47
JoeMartinRince <joemartinrince2028@cs.sjcetpalai.ac.in>
2024-12-17 03:57:53 +0530

    exp1.py

--- exp1.py
+++ exp1.py
@@ -1,6 +1,7 @@
 """
 AUTHOR : JOE MARTIN RINCE
 DATE : 05-10-2024
+Write a Python program that asks the user to input their name and age. After receiving the input, display a message that includes the user's name and age.
 """
 name=input("ENTER YOUR NAME : ")
 age=input("ENTER YOUR AGE: ")
